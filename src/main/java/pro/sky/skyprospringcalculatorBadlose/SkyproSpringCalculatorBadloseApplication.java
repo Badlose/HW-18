4dad4ad3ef6096c41f58c6d42dadf58717596840
Badlose <git@github.com:Badlose/helloworld.git>
2024-09-23 17:45:13 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringcalculatorBadlose/SkyproSpringCalculatorBadloseApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringcalculatorBadlose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringCalculatorBadloseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringCalculatorBadloseApplication.class, args);
+	}
+
+}
